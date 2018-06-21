621d6cb9557f2193812cc14ab5079f017b36ae70
liangxuecong <liangxuecong@sensenets.com>
2018-06-22 00:40:09 +0800

    1.补全3种电影的 movie-list-template

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,15 +1,19 @@
 var app = getApp();
 Page({
   data: {
-    movies: []
+    //movies: []
   },
   onLoad(event) {
     let inTheatersUrl = app.data.globalData.doubanBase + "/v2/movie/in_theaters";
     var comingSoonUrl = app.data.globalData.doubanBase + "/v2/movie/coming_soon";
-    var top250Url = app.data.globalData.doubanBase + "/v2/movie/top250 ";
-    this.getMovieListData(comingSoonUrl);
+    var top250Url = app.data.globalData.doubanOffical + "/v2/movie/top250 ";
+    
+    this.getMovieListData(inTheatersUrl, "inTheaters");
+    this.getMovieListData(comingSoonUrl, "comingSoon");
+    //this.getMovieListData(top250Url, "top250");
+    console.log("onload")
   },
-  getMovieListData(url) {
+  getMovieListData(url, dataKey) {
     var self = this;
     wx.request({
       url: url,
@@ -22,7 +26,7 @@ Page({
       },
       method: 'GET',
       success(res) {
-        self.processDoubanData(res.data);
+        self.processDoubanData(res.data, dataKey);
       },
       fail() {
 
@@ -35,7 +39,7 @@ Page({
   /**
    * 处理服务器返回的数据
    */
-  processDoubanData(moviesDouban) {
+  processDoubanData(moviesDouban, dataKey) {
     var movies = [];
     console.log(moviesDouban)
     for (var idx in moviesDouban.subjects) {
@@ -51,9 +55,11 @@ Page({
         movieId: subject.id
       }
       movies.push(temp);
-      this.setData({
-        movies: movies
-      })
+
+      var curData = {};
+      curData[dataKey] = movies;
+
+      this.setData(curData);
     }
   }
 })
